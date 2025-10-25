e24b2d864d8a2f54fd59a18c1f61797af3897d56
Eisendrachen <firessscalesss@gmail.com>
2025-10-25 14:37:39 -0500

    More basics

---
+++ src/main/java/hospital/records/HospitalRecordsApp.java
@@ -0,0 +1,13 @@
+package hospital.records;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalRecordsApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalRecordsApp.class, args);
+	}
+
+}
