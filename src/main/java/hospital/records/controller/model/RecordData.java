a25ac48a96d1f3acaac2f7545d4459718e400163
Eisendrachen <firessscalesss@gmail.com>
2025-11-12 21:23:11 -0600

    Fixed the problem with the Record DTO

--- src/main/java/hospital/records/controller/model/RecordData.java
+++ src/main/java/hospital/records/controller/model/RecordData.java
@@ -1,6 +1,7 @@
 package hospital.records.controller.model;
 
 import hospital.records.entity.Patient;
+import hospital.records.entity.PatientRecord;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,10 +12,19 @@ public class RecordData {
 	private String diagnosis;
 	private Patient patient;
 	
-	public RecordData(Record record) {
+	public RecordData(PatientRecord record) {
 		this.recordId = record.getRecordId();
 		this.diagnosis = record.getDiagnosis();
 		this.patient = record.getPatient();
+	}
+	
+	public PatientRecord toRecord() {
+		PatientRecord record = new PatientRecord();
 		
+		record.setRecordId(recordId);
+		record.setDiagnosis(diagnosis);
+		record.setPatient(patient);
+		
+		return record;
 	}
 }
