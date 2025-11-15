82e1b5e6974e783f1bbf18e49e09b36e0da38988
Eisendrachen <firessscalesss@gmail.com>
2025-11-15 17:52:49 -0600

    Updated test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,63 @@
+DROP TABLE IF EXISTS doctor_patient;
+DROP TABLE IF EXISTS record;
+DROP TABLE IF EXISTS patient;
+DROP TABLE IF EXISTS doctor;
+DROP TABLE IF EXISTS office;
+
+CREATE TABLE office (
+	office_id int NOT NULL AUTO_INCREMENT,
+	office_location varchar(128) NOT NULL,
+	office_name varchar(128) NOT NULL,
+	office_zip varchar(10),
+	office_phone varchar(24),
+	PRIMARY KEY (office_id)
+);
+
+CREATE TABLE doctor (
+	doctor_id int NOT NULL AUTO_INCREMENT,
+	office_id int NOT NULL,
+	doctor_name varchar(128) NOT NULL,
+	doctor_specialty varchar(128),
+	PRIMARY KEY (doctor_id),
+	FOREIGN KEY (office_id) REFERENCES office (office_id) ON DELETE CASCADE
+);
+
+CREATE TABLE patient (
+	patient_id int NOT NULL AUTO_INCREMENT,
+	patient_name varchar(128) NOT NULL,
+	patient_insurance varchar(128),
+	patient_phone varchar(24),
+	patient_address varchar(128),
+	PRIMARY KEY (patient_id)
+);
+
+CREATE TABLE record (
+	record_id int NOT NULL AUTO_INCREMENT,
+	patient_id int NOT NULL,
+	diagnosis TEXT(1000),
+	PRIMARY KEY (record_id),
+	FOREIGN KEY (patient_id) REFERENCES patient (patient_id) ON DELETE CASCADE
+);
+
+CREATE TABLE doctor_patient (
+	doctor_id int NOT NULL,
+	patient_id int NOT NULL,
+	FOREIGN KEY (doctor_id) REFERENCES doctor (doctor_id) ON DELETE CASCADE,
+	FOREIGN KEY (patient_id) REFERENCES patient (patient_id) ON DELETE CASCADE
+);
+
+INSERT INTO office
+(office_id, office_location, office_name, office_zip, office_phone)
+VALUES(1, 'Test Place 1', 'Office Test 1', '11111', '222 111 3333');
+
+INSERT INTO office
+(office_id, office_location, office_name, office_zip, office_phone)
+VALUES(2, 'Test Place 2', 'Office Test 2', '99932', '421 523 1453');
+
+INSERT INTO office
+(office_id, office_location, office_name, office_zip, office_phone)
+VALUES(3, 'Test Place 3', 'Office Test 3', '52553', '023 395 1050');
+
 INSERT INTO doctor
 (doctor_id, office_id, doctor_name, doctor_specialty)
 VALUES(1, 1, 'John Test', 'Testiology');
@@ -10,24 +70,9 @@ INSERT INTO doctor
 (doctor_id, office_id, doctor_name, doctor_specialty)
 VALUES(3, 2, 'Dawn Thebest', 'Testiology');
 
-INSERT INTO doctor_patient
-(doctor_id, patient_id)
-VALUES(1, 1);
-
-INSERT INTO doctor_patient
-(doctor_id, patient_id)
-VALUES(1, 2);
-
-INSERT INTO doctor_patient
-(doctor_id, patient_id)
-VALUES(2, 3);
-
-INSERT INTO office
-(office_id, office_location, office_name, office_zip, office_phone)
-VALUES(1, 'Test Place 1', 'Office Test 1', '11111', '222 111 3333');
-INSERT INTO office
-(office_id, office_location, office_name, office_zip, office_phone)
-VALUES(2, 'Test Place 2', 'Office Test 2', '99932', '421 523 1453');
+INSERT INTO doctor
+(doctor_id, office_id, doctor_name, doctor_specialty)
+VALUES(4, 3, 'Gave Up', 'Testiology');
 
 INSERT INTO patient
 (patient_id, patient_name, patient_insurance, patient_phone, patient_address)
@@ -41,6 +86,26 @@ INSERT INTO patient
 (patient_id, patient_name, patient_insurance, patient_phone, patient_address)
 VALUES(3, 'The Tested', 'Dental', '049 104 1503', 'Home 3');
 
+INSERT INTO patient
+(patient_id, patient_name, patient_insurance, patient_phone, patient_address)
+VALUES(4, 'Gael Knight', 'Immortality', '425 104 1953', 'Ringed City');
+
+INSERT INTO doctor_patient
+(doctor_id, patient_id)
+VALUES(1, 1);
+
+INSERT INTO doctor_patient
+(doctor_id, patient_id)
+VALUES(1, 2);
+
+INSERT INTO doctor_patient
+(doctor_id, patient_id)
+VALUES(2, 3);
+
+INSERT INTO doctor_patient
+(doctor_id, patient_id)
+VALUES(4, 4);
+
 INSERT INTO record
 (record_id, patient_id, diagnosis)
 VALUES(1, 1, 'Nothing');
@@ -52,3 +117,7 @@ VALUES(2, 2, 'Death');
 INSERT INTO record
 (record_id, patient_id, diagnosis)
 VALUES(3, 3, 'Boring');
+
+INSERT INTO record
+(record_id, patient_id, diagnosis)
+VALUES(4, 4, 'Hollowing');
